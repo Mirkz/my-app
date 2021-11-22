0f7bbe8e82d4a854840fb81689c14063ae454521
M1rk095 <87029116+M1rk095@users.noreply.github.com>
2021-11-22 22:49:45 +0100

    added addNewRecord

--- src/app/match/match-table/match-table.component.ts
+++ src/app/match/match-table/match-table.component.ts
@@ -47,7 +47,25 @@ export class MatchTableComponent implements OnInit {
 
 
   addNewRecord(): void {
+    let teams = this.datasource.map(record => record.homeTeam).concat(this.datasource.map(record => record.awayTeam));
+    teams = Array.from(new Set(teams)); // remove duplicates
+    const homeTeam = teams[Math.round(Math.random() * (teams.length - 1))]; // pick random team among the previous ones
+    let awayTeam: string;
+    do {
+      awayTeam = teams[Math.round(Math.random() * (teams.length - 1))];
+    } while (homeTeam === awayTeam);
 
+    const now = new Date();
+    
+    this.datasource.push({
+      id: this.datasource[this.datasource.length - 1].id + 2,
+      homeTeam: homeTeam,
+      awayTeam: awayTeam,
+      date: `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear() - 2000}`,
+      time: `${now.getHours()}:${now.getMinutes()}`,
+      homeGoals: Math.round(Math.random() * 5),
+      awayGoals: Math.round(Math.random() * 5)
+    });
   }
 
 }
