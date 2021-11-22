3ed6bafddc5609e9c3ea6f149e25578e5e8036fa
M1rk095 <87029116+M1rk095@users.noreply.github.com>
2021-11-22 22:16:45 +0100

    removed interval

--- src/app/match/match-table/match-table.component.ts
+++ src/app/match/match-table/match-table.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { MatchService } from '../match.service';
 
 @Component({
@@ -7,45 +6,31 @@ import { MatchService } from '../match.service';
   templateUrl: './match-table.component.html',
   styleUrls: ['./match-table.component.css']
 })
-export class MatchTableComponent implements OnInit, OnDestroy {
-
-  private subscription: any = undefined;
-  private intervalId: any = undefined;
-  public updateTimer = 1;
+export class MatchTableComponent implements OnInit {
 
   public datasource: any = undefined;
 
+
   constructor(private matchService: MatchService) { }
 
+
   ngOnInit(): void {
-    // retrieve data every 5 seconds
-    this.intervalId = setInterval(
-      () => {
-        this.updateTimer -= 1;
-        if (this.updateTimer === 0) {
-          this.retrieveData();
-          this.updateTimer = 5;
-        }
-      },
-      1000
-    );
-  }
-
-
-  ngOnDestroy(): void {
-    clearInterval(this.intervalId);
+    this.retrieveData();
   }
 
 
   private retrieveData() {
-    if (this.subscription)
-      this.subscription.unsubscribe();
-    this.subscription = this.matchService.getData().subscribe(
+    this.matchService.getData().subscribe(
       {
         next: resp => this.datasource = resp,
         error: e => console.error(e)
       }
     );
+  }
+
+
+  addNewRecord() {
+
   }
 
 }
